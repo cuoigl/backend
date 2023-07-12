ce6b63d279c6eb1ed150861650c98adfdd5339ee
Cuoi <cuoi@Nguyens-MacBook-Pro.local>
2023-07-12 08:48:50 +0700

    Admin Can Update Product

--- controllers/productController.js
+++ controllers/productController.js
@@ -182,6 +182,33 @@ const adminCreateProduct = async (req, res, next) => {
   }
 };
 
+const adminUpdateProduct = async (req, res, next) => {
+  try {
+    const product = await Product.findById(req.params.id).orFail();
+    const { name, description, count, price, category, attributesTable } =
+      req.body;
+    product.name = name || product.name;
+    product.description = description || product.description;
+    product.count = count || product.count;
+    product.price = price || product.price;
+    product.category = category || product.category;
+    if (attributesTable.length > 0) {
+      product.attrs = [];
+      attributesTable.map((item) => {
+        product.attrs.push(item);
+      });
+    } else {
+      product.attrs = [];
+    }
+    await product.save();
+    res.json({
+      message: "product updated",
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
 module.exports = {
   getProducts,
   getProductById,
@@ -189,4 +216,5 @@ module.exports = {
   adminGetProducts,
   adminDeleteProduct,
   adminCreateProduct,
+  adminUpdateProduct,
 };
