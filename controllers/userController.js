7382dec820dee9bf0abee6f11320b4ace97a24dd
Cuoi <cuoi@Nguyens-MacBook-Pro.local>
2023-07-12 09:01:01 +0700

    Register New User

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,4 +8,29 @@ const getUsers = async (req, res, next) => {
     next(err);
   }
 };
-module.exports = getUsers;
+
+const registerUser = async (req, res, next) => {
+  try {
+    const { name, lastName, email, password } = req.body;
+    if (!(name && lastName && email && password)) {
+      return res.status(400).send("All inputs are required");
+    }
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ error: "user exists" });
+    } else {
+      const user = await User.create({
+        name,
+        lastName,
+        email: email.toLowerCase(),
+        password: password,
+      });
+      res.status(201).send(user);
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { getUsers, registerUser };
